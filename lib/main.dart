9e4206f6381596508dc52ee59ea0fefa80258d7d
Ritesh <ritesht@bigrattle.com>
2021-10-24 21:07:07 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyGetXApp());
+}
+
+class MyGetXApp extends StatefulWidget {
+  const MyGetXApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyGetXAppState createState() => _MyGetXAppState();
+}
+
+class _MyGetXAppState extends State<MyGetXApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
